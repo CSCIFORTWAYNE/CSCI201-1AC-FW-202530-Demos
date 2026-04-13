2eee90a1caf5a1f278b839c95b461e786a35156b
Angela Venable <avenable11@ivytech.edu>
2026-04-13 18:41:53 -0400

    templated function

--- 04_13_26/main.cpp
+++ 04_13_26/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include "drink.h"
 
 int max(int, int);
 double max(double, double);
 char max(char, char);
 
+template <class t>
+t maxTemplate(t, t);
+
 int main()
 {
+    int x = 8;
+    int y = 4;
+    std::cout << max(x, y) << std::endl;
+    std::cout << maxTemplate(x, y) << std::endl;
+
+    drink d1(COFFEE, drink::HOT, sizeType::LARGE, "Milk", "Chocolate");
+    drink d2(COFFEE, drink::ICE, sizeType::LARGE, "Milk", "Chocolate");
+    std::cout << d1 << std::endl;
+    std::cout << d2 << std::endl;
+    std::cout << maxTemplate(d1, d2).toString() << std::endl;
     return 0;
 }
 
@@ -34,6 +48,19 @@ char max(char a, char b)
 }
 
 double max(double a, double b)
+{
+    if (a >= b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+template <class t>
+t maxTemplate(t a, t b)
 {
     if (a >= b)
     {
